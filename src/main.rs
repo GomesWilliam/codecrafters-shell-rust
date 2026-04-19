6829311620af55cb2cae17c0679c1b8a05c36b53
William Gomes <gomes.souza@live.com>
2026-04-19 02:50:39 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,10 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    //Read user input and print it back to the console
+    let mut command: String = String::new();
+    io::stdin().read_line(&mut command).expect("Failed to read line");
+
+    println!("{command}: command not found");
 }
