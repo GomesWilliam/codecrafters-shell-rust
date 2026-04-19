491dd7dead1976a0f488bd986cb8323c9fa838f1
William Gomes <gomes.souza@live.com>
2026-04-19 03:21:53 +0200

    codecrafters submit [skip ci]

--- src/commands.rs
+++ src/commands.rs
@@ -1,8 +1,25 @@
 //Command handling logic
-pub fn handle_command(command: String) {
+pub fn handle_command(command: String, args: Vec<String>) {
     if command.trim() == "exit" {
         std::process::exit(0);
     }
+    else if command.trim() == "echo" {
+        println!("{}", args.join(" "));
+    }
+    else {
+        println!("{}: command not found", command.trim());
+    }
 
-    println!("{}: command not found", command.trim());
+}
+
+pub fn parse_command(input: String) -> (String, Vec<String>) {
+    let parts: Vec<String> = input.trim().split_whitespace().map(String::from).collect();
+    if parts.is_empty() {
+        return (String::new(), Vec::new());
+    }
+
+    let command: String  = parts[0].clone();
+    let args: Vec<String> = parts[1..].to_vec();
+
+    (command, args)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use std::io::{self, Write};
 mod commands;
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -13,6 +11,10 @@ fn main() {
         io::stdin().read_line(&mut command).expect("Failed to read line");
 
         //Passes the command to the command handler
-        commands::handle_command(command);
+
+        // Parse the command and its arguments
+        let (command, args) = commands::parse_command(command);
+
+        commands::handle_command(command, args);
     }
 }
